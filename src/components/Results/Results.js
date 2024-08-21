e860cf8aa4da4c247861392f4cbdb3ab6c376676
Andreea Buliga <abuliga02@gmail.com>
2024-08-21 15:52:36 +0100

    Alt added

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -6,7 +6,7 @@ function Results ({isGameWon, word, handleClick}) {
     if (isGameWon) {
         return (
             <div className="result">
-                <img src="/images/victory.gif"/>
+                <img src="/images/victory.gif" alt="Congratulations! You Won!"/>
                 <h2>Congratulations!</h2> 
                 <p>You guessed the correct word.</p>
                 <button onClick={handleClick}>PLAY AGAIN</button>
@@ -15,7 +15,7 @@ function Results ({isGameWon, word, handleClick}) {
     } else {
         return (
             <div className="result">
-                <img src="/images/lost.gif"/>
+                <img src="/images/lost.gif" alt="You lost!"/>
                 <h2>Game Over!</h2> 
                 <p>The correct word was: {word}.</p>
                 <button className="resultButton" onClick={handleClick}>PLAY AGAIN</button>
