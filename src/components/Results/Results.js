57aab2e892d96c8df053d420c7aaaa47dfce44c6
Andreea Buliga <abuliga02@gmail.com>
2024-08-21 16:05:28 +0100

    Edit Results component

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,27 +1,25 @@
 import React from "react";
-import './Results.css'
+import './Results.css';
 
-function Results ({isGameWon, word, handleClick}) {
-    
-    if (isGameWon) {
-        return (
-            <div className="result">
-                <img src="/images/victory.gif" alt="Congratulations! You Won!"/>
-                <h2>Congratulations!</h2> 
-                <p>You guessed the correct word.</p>
-                <button className="resultButton" onClick={handleClick}>PLAY AGAIN</button>
-            </div>
-        )   
-    } else {
-        return (
-            <div className="result">
-                <img src="/images/lost.gif" alt="You lost!"/>
-                <h2>Game Over!</h2> 
-                <p>The correct word was: {word}.</p>
-                <button className="resultButton" onClick={handleClick}>PLAY AGAIN</button>
-            </div>
-        )
-    }
+function Results ({ isGameWon, word, handleClick }) {
+    return (
+        <div className="result">
+            {isGameWon ? (
+                <>
+                    <img src="/images/victory.gif" alt="Congratulations! You Won!" />
+                    <h2>Congratulations!</h2> 
+                    <p>You guessed the correct word.</p>
+                </>
+            ) : (
+                <>
+                    <img src="/images/lost.gif" alt="You lost!" />
+                    <h2>Game Over!</h2> 
+                    <p>The correct word was: {word.join('')}.</p>
+                </>
+            )}
+            <button className="resultButton" onClick={handleClick}>PLAY AGAIN</button>
+        </div>
+    );
 }
 
 export default Results;
